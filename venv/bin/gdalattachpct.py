8c68bbe2e5ad67e135b4035c44a9afd4005d18d1
Barthélemy Turri <bart@MacBook-Air-de-Barthelemy-138.local>
2025-12-23 22:20:20 +0100

    Ajout des fonctions de régression linéaire et analyse par région

---
+++ venv/bin/gdalattachpct.py
@@ -0,0 +1,14 @@
+#!/Users/bart/Desktop/Projet Python/Python_Project_2A/venv/bin/python3.11
+
+import sys
+
+from osgeo.gdal import UseExceptions, deprecation_warn
+
+# import osgeo_utils.gdalattachpct as a convenience to use as a script
+from osgeo_utils.gdalattachpct import *  # noqa
+from osgeo_utils.gdalattachpct import main
+
+UseExceptions()
+
+deprecation_warn("gdalattachpct")
+sys.exit(main(sys.argv))
